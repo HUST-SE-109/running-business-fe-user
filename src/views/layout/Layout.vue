1fbe180ebede552ed0604940799b7729c1048e60
wanzizhe <1278133518@qq.com>
2018-05-07 19:50:15 +0800

    add Home component fix Footer component

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -28,7 +28,7 @@ export default {
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
-    line-height: 160px;
+    line-height: 0;
   }
 
   body > .el-container {
--- src/views/layout/components/Footer.vue
+++ src/views/layout/components/Footer.vue
@@ -1,61 +1,75 @@
 <template>
   <el-footer>
-      <el-row :gutter="10">
-          <el-col :md="4" ><div class="grid-content bg-purple">
-              <h5>关于我们</h5>
-              <ul>
-                <li>跑腿介绍</li>
-                <li>媒体报道</li>
-                <li>联系跑腿</li>
-              </ul>
-            </div></el-col>
-          <el-col :md="4" ><div class="grid-content bg-purple-light">
-            <h5>意见反馈</h5>
-              <ul>
-                <li>常见问题</li>
-                <li>如何下单</li>
-                <li>如何付费</li>
-                <li>如何开具发票</li>
-              </ul>
-            </div></el-col>
-          <el-col :md="4" ><div class="grid-content bg-purple">
-            <h5>网站导航</h5>
-              <ul>
-                <li>在线下单</li>
-                <li>招商加盟</li>
-                <li>加入跑男</li>
-                <li>开放平台</li>
-                <li>外卖助手</li>
-              </ul>
-            </div></el-col>
-          <el-col :md="4" ><div class="grid-content bg-purple-light">
-            <h5>7x24小时客服电话</h5>
-            <h3>400-6997-990</h3>
-            <h5>电子邮箱</h5>
-            <h5>paotui@outlook.com</h5>
-            </div></el-col>
-          <el-col :md="4" ><div class="grid-content bg-purple">
-            <h5>官方微信</h5>
-            <img src="../../../../src/assets/weixin.jpg" alt="">
-            <div class="icon">
-                <img src="../../../../src/assets/weixinimg.jpg" alt="" class="smallIcon">
-                <span>关注微信</span>
-            </div>
-            </div></el-col>
-          <el-col :md="4" ><div class="grid-content bg-purple-light">
-            <h5>官方微博</h5>
-            <img src="../../../../src/assets/weibo.jpg" alt="">
-            <div class="icon">
-              <img src="../../../../src/assets/weiboimg.jpg" alt="" class="smallIcon">
-              <span>关注微博</span>
-            </div>
-            </div></el-col>
-       </el-row>
-       <el-row class="footer">
-          <el-col :md="24"><div class="grid-content bg-purple-light">
-            <p>跑腿 www.paotui.com 版权所有：哈尔滨CDIO技术实验室@2015-2018 黑ICP备150099674-7</p>
-            </div></el-col>
-       </el-row>
+    <el-row :gutter="10">
+      <el-col :md="4">
+        <div class="grid-content bg-purple">
+          <h4>关于我们</h4>
+          <ul>
+            <li>跑腿介绍</li>
+            <li>媒体报道</li>
+            <li>联系跑腿</li>
+          </ul>
+        </div>
+      </el-col>
+      <el-col :md="4">
+        <div class="grid-content bg-purple-light">
+          <h4>意见反馈</h4>
+          <ul>
+            <li>常见问题</li>
+            <li>如何下单</li>
+            <li>如何付费</li>
+            <li>如何开具发票</li>
+          </ul>
+        </div>
+      </el-col>
+      <el-col :md="4">
+        <div class="grid-content bg-purple">
+          <h4>网站导航</h4>
+          <ul>
+            <li>在线下单</li>
+            <li>招商加盟</li>
+            <li>加入跑男</li>
+            <li>开放平台</li>
+            <li>外卖助手</li>
+          </ul>
+        </div>
+      </el-col>
+      <el-col :md="4">
+        <div class="grid-content bg-purple-light">
+          <h4>7x24小时客服电话</h4>
+          <h3>400-6997-990</h3>
+          <h4>电子邮箱</h4>
+          <h4>paotui@outlook.com</h4>
+        </div>
+      </el-col>
+      <el-col :md="4">
+        <div class="grid-content bg-purple">
+          <h4>官方微信</h4>
+          <img class="weixinImg" src="@/assets/weixin.jpg" alt="微信图片">
+          <div>
+            <img class="weixinIcon" src="@/assets/weixinimg.jpg" alt="微信图标">
+            <span>关注微信</span>
+          </div>
+        </div>
+      </el-col>
+      <el-col :md="4">
+        <div class="grid-content bg-purple-light">
+          <h4>官方微博</h4>
+          <img class="weiboImg" src="@/assets/weibo.jpg" alt="微博图片">
+          <div>
+            <img class="weiboIcon" src="@/assets/weiboimg.jpg" alt="微博图标">
+            <span>关注微博</span>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row class="copyright">
+      <el-col :md="24">
+        <div class="grid-content bg-purple-light">
+          <p>跑腿 www.paotui.com 版权所有：哈尔滨CDIO技术实验室@2015-2018 黑ICP备150099674-7</p>
+        </div>
+      </el-col>
+    </el-row>
   </el-footer>
 </template>
 
@@ -65,53 +79,47 @@ export default {
 };
 </script>
 <style lang="less">
-ul {
-  list-style: none;
-}
-* {
-  margin: 0;
-  padding: 0;
-}
-li {
-  cursor: pointer;
-}
-li:hover {
-  color: #aaa;
-}
-p {
-  color: #aaa;
-}
 .el-footer {
   padding: 0;
   text-align: center;
   line-height: 50px;
-}
-.el-col {
-  // border-radius: 4px;
-}
-.bg-purple-dark {
-  // background: #99a9bf;
-}
-.bg-purple {
-  // background: #d3dce6;
-}
-.bg-purple-light {
-  // background: #e5e9f2;
-}
-.grid-content {
-  // border-radius: 4px;
-  width: 100%;
-  min-height: 36px;
-}
-.smallIcon {
-  width: 30px;
-  height: 30px;
-}
-.icon span{
-  font-size: 10px;
-}
-.footer {
-  width: 100%;
-  background: #ddd;
+  .grid-content {
+    width: 100%;
+    min-height: 36px;
+    .weixinImg,
+    .weiboImg {
+      width: 100px;
+      height: 100px;
+    }
+    .weixinIcon,
+    .weiboIcon {
+      width: 30px;
+      height: 30px;
+    }
+    span {
+      font-size: 10px;
+    }
+  }
+  h4 {
+    color: #303133;
+  }
+  ul {
+    padding-left: 0;
+    list-style: none;
+  }
+  li {
+    color: #303133;
+    cursor: pointer;
+  }
+  li:hover{
+      color: #909399;
+  }
+  h3 {
+    color: #303133;
+  }
+  .copyright {
+    width: 100%;
+    background: #ddd;
+  }
 }
 </style>
